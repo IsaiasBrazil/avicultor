8f7130f9d828850d6faf296c53beb17b1ece76e7
feli0x1 <sdiylaogzbhk@proton.me>
2023-05-24 10:24:31 -0300

    Criação do arquivo responsável pelos sensores

---
+++ lib/cadastrar_sensor.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TelaCadastroSensor extends StatefulWidget {
+  const TelaCadastroSensor({super.key});
+
+  @override
+  State<TelaCadastroSensor> createState() => _TelaCadastroSensorState();
+}
+
+class _TelaCadastroSensorState extends State<TelaCadastroSensor> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
