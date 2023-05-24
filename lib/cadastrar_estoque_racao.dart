3183dcf1f9b9bbf18c4ab05c9ccce4649bf307be
feli0x1 <sdiylaogzbhk@proton.me>
2023-05-24 10:16:34 -0300

    Criação do arquivo responsável pelo estoque de ração

---
+++ lib/cadastrar_estoque_racao.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TelaCadastroEstoqueRacao extends StatefulWidget {
+  const TelaCadastroEstoqueRacao({super.key});
+
+  @override
+  State<TelaCadastroEstoqueRacao> createState() => _TelaCadastroEstoqueRacaoState();
+}
+
+class _TelaCadastroEstoqueRacaoState extends State<TelaCadastroEstoqueRacao> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
