e2ddb26b7451a3149860db1a11050d6ce436e3fb
feli0x1 <sdiylaogzbhk@proton.me>
2023-05-24 00:29:38 -0300

    widgets ajustados para permitir navegação entre páginas ao chamar onTap

--- lib/widget_drawer.dart
+++ lib/widget_drawer.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tcc/cadastrar_lote.dart';
+import 'home.dart';
 import 'widget_listtile.dart';
 
 class MenuLateral extends StatelessWidget {
@@ -20,12 +22,25 @@ class MenuLateral extends StatelessWidget {
                   child: Text(
             titulo,
             style: TextStyle(
-              fontFamily: 'BebasNeue', 
-              fontSize: 28, color: Colors.white),
+                fontFamily: 'BebasNeue', fontSize: 28, color: Colors.white),
           ))),
-          ListTileCustom(nome: 'Página inicial'),
-          ListTileCustom(nome: 'Cadastro de galpões'),
-          ListTileCustom(nome: 'Cadastro de Lotes'),
+          ListTileCustom(
+            nome: 'Página inicial',
+            onTap: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => Home()));
+            },
+          ),
+          ListTileCustom(
+            nome: 'Cadastro de galpões',
+          ),
+          ListTileCustom(
+            nome: 'Cadastro de Lotes',
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => TelaCadastroLote()));
+            },
+          ),
           ListTileCustom(nome: 'Controle de Lotes'),
           ListTileCustom(nome: 'Estoque de Ração'),
           ListTileCustom(nome: 'Cadastro de Sensores')
--- lib/widget_listtile.dart
+++ lib/widget_listtile.dart
@@ -3,13 +3,16 @@ import 'package:flutter/material.dart';
 class ListTileCustom extends StatelessWidget {
   final String nome;
   final IconData? icone;
-  const ListTileCustom({super.key, required this.nome, this.icone});
+  final VoidCallback? onTap;
+  const ListTileCustom({super.key, required this.nome, this.icone, this.onTap});
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
         leading: icone != null ? Icon(icone) : Icon(Icons.home),
         title: Text(nome,
-            style: TextStyle(fontFamily: 'BebasNeue', fontSize: 22, color: Colors.white)));
+            style: TextStyle(
+                fontFamily: 'BebasNeue', fontSize: 22, color: Colors.white)),
+        onTap: onTap);
   }
 }
